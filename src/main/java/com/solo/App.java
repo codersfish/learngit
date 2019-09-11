e66aae946ab6ea38c638fd0be10a44e17560cad2
ANDY <a@a.com>
2019-09-11 14:30:47 +0800

    fdsafd

--- src/main/java/com/solo/App.java
+++ src/main/java/com/solo/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        System.out.println( "123" );
+        System.out.println( "123 fsda" );
 
         System.out.println(smExcelUtils.ReadExcel("c://tt.xlsx",0));
 
