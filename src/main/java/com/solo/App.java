4a61396ad9058dd6e290f32351dc1f0dbd3623cc
ANDY <a@a.com>
2019-09-12 14:25:23 +0800

    Merge branch 'master' of https://github.com/nutcollect/learngit into local01
    # Conflicts:
    #	src/main/java/com/solo/App.java

--- src/main/java/com/solo/App.java
+++ src/main/java/com/solo/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        System.out.println( "123 fd fdsa yt 555  456" );
+        System.out.println( "123 fd fdsa yt 555 jj" );
 
         System.out.println(smExcelUtils.ReadExcel("c://tt.xlsx",0));
 
